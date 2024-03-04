b644d654746ad0cf8e6e7f8d35b324aa817c33c2
Sean Connolly <sean.connolly@northwestern.edu>
2024-03-04 02:17:43 +0000

    Update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,8 @@
 class UsersController < ApplicationController
+  def show
+    @user = User.find_by ({ "id" => params["id"] })
+  end
+
   def new
   end
 
@@ -7,7 +11,7 @@ class UsersController < ApplicationController
       @user = User.new
       @user["first_name"] = params["first_name"]
       @user["last_name"] = params["last_name"]
-      @user["email"] = params["email"]
+      @user["email"] = params["email"] 
       @user["password"] = BCrypt::Password.create(params["password"])
       @user.save
       redirect_to "/login"
